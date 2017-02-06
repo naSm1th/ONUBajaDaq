472ad150f2ab564f3eac036ad43fbe1c8269bc4c
Ryan Carl <rcarl94@gmail.com>
2017-02-06 11:58:03 -0600

    added checksum validation, started file handling

--- src/logger.c
+++ src/logger.c
@@ -38,7 +38,7 @@ void writeToFile() {
 }
 
 char *waitForSerial() {
-    return "$GPRMC,12:34:56.00,A,5133.82,N,00042.24,W,173.8,231.8,030217,004.2,W*70";
+    return "$GPRMC,081836,A,3751.65,S,14507.36,E,000.0,360.0,130998,011.3,E*62";
 }
 
 char *formatMetric(int *values, int numVal, char *time) {
@@ -62,10 +62,10 @@ char *formatMetric(int *values, int numVal, char *time) {
 }
 
 main(int argc, char *argv[]) {
-    /* metric string */
-    char *met;
     /* file pointer for output */
     FILE *fp;
+    int filenum = 1;
+    char filename[8]; 
 
     pthread_t thread;
     pthread_attr_t attr;
@@ -83,15 +83,15 @@ main(int argc, char *argv[]) {
         printf("ERROR in pthread_join()");
     }
 
-    fp = fopen("test.csv", "a+");
-    /* 
-       1. wait for serial input
-       2. read counts and reset array 
-       3. write to file
-    */
+    // open new file
+    sprintf(filename, "%03d.csv", filenum);
+    fp = fopen(filename, "a+");
 
     char *rawgps = (char *) malloc(MAX_LEN);
     strcpy(rawgps, waitForSerial());
+    char *csum_str = (char *) malloc(strlen(rawgps));
+    // copy for checksum comparison (omit $)
+    strcpy(csum_str, rawgps+1);
     char **gpstok = (char **) malloc(MAX_TOKENS*sizeof(char *));
     *gpstok = strtok(rawgps, ",");    /* split string */
     int i = 0;
@@ -100,30 +100,53 @@ main(int argc, char *argv[]) {
         gpstok[++i] = strtok(NULL, ",");
     }
 
-    // get stuff from gps string
-    char *gpsstr = (char *) malloc(MAX_LEN);
-    // time
-    strcpy(gpsstr,gpstok[1]);
-    strcat(gpsstr,",");
-    // if valid fix, keep coordinates
-    if (strcmp(gpstok[2],"A") == 0) {
-        snprintf(gpsstr+strlen(gpsstr),MAX_LEN-strlen(gpsstr),"%s %s,%s %s,",gpstok[3],gpstok[4],gpstok[5],gpstok[6]);
-    } else {
-        sprintf(gpsstr+strlen(gpsstr),",,");
+    // check sentence type
+    if (strcmp(gpstok[0], "$GPRMC") == 0) {
+        // checksum calc
+        int csum = 0;
+        int csum_calc = 0;
+        if (csum = (int)strtol(gpstok[11]+2, NULL, 16)) {
+            int j, k;
+            for (j = 0; j < strlen(csum_str); j++) {
+                // skip checksum
+                if (csum_str[j] != '*')
+                    csum_calc = csum_calc ^ csum_str[j];
+                // exit if at checksum
+                else
+                    break;
+                //printf("%c\n", csum_str[j]);
+            }
+            //printf("%i = %i\n", csum_calc, csum);
+        }
+        // if checksum is valid, keep values
+        if (csum_calc == csum) {
+            // parse gps string
+            char *gpsstr = (char *) malloc(MAX_LEN);
+            // date and time
+            strcpy(gpsstr,gpstok[1]);
+            strcat(gpsstr,",");
+            // if valid fix, keep coordinates
+            if (strcmp(gpstok[2],"A") == 0) {
+                snprintf(gpsstr+strlen(gpsstr),MAX_LEN-strlen(gpsstr),"%s %s,%s %s,",gpstok[3],gpstok[4],gpstok[5],gpstok[6]);
+            } else {
+                sprintf(gpsstr+strlen(gpsstr),",,");
+            }
+            double speed = 0;
+            sscanf(gpstok[7], "%lf", &speed);
+            speed = speed*6076.0/5280.0;
+            snprintf(gpsstr+strlen(gpsstr),MAX_LEN-strlen(gpsstr),"%lf",speed);
+            printf("%s", gpsstr);
+            // format for google maps: +40  42.6142', -74  00.4168'
+        } else {
+            // invalid checksum
+        }
     }
-    double speed = 0;
-    sscanf(gpstok[7], "%lf", &speed);
-    speed = speed*6076.0/5280.0;
-    snprintf(gpsstr+strlen(gpsstr),MAX_LEN-strlen(gpsstr),"%lf,",speed);
-    printf("%s", gpsstr);
-    // format for google maps: +40  42.6142', -74  00.4168'
-    //met = formatMetric(counts, 8, );
-    //fprintf(fp, "%s\n", met);
+
+    // read counts from USBDaq
 
     // free allocated memory
     free(rawgps);
     free(gpstok);
-    //free(met);
 
     fclose(fp);
     /* close thread */
