fe6f2f608fbbc1f895919239b3d8144a3aa31fd5
Ryan Carl <rcarl94@gmail.com>
2017-02-26 17:58:44 -0600

    Fixed email ip

--- src/email_ip.py
+++ src/email_ip.py
@@ -24,7 +24,7 @@ def connect_type(word_list):
 
 # Change to your own account information
 # Account Information
-to = 'nthepianoman@email.com;rcarl94@gmail.com' # Email to send to.
+to = 'nthepianoman@gmail.com,rcarl94@gmail.com' # Email to send to.
 gmail_user = 'onubajadaq@gmail.com' # Email to send from. (MUST BE GMAIL)
 gmail_password = 'onub@j@d@q' # Gmail password.
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587) # Server to use.
@@ -43,25 +43,25 @@ data = p.communicate()  # Get data from 'p terminal'.
 # Split IP text block into three, and divide the two containing IPs into words.
 ip_lines = data[0].splitlines()
 split_line_a = ip_lines[1].split()
-split_line_b = ip_lines[2].split()
+# split_line_b = ip_lines[2].split()
 
 # con_type variables for the message text. ex) 'ethernet', 'wifi', etc.
 ip_type_a = connect_type(split_line_a)
-ip_type_b = connect_type(split_line_b)
+#ip_type_b = connect_type(split_line_b)
 
 """Because the text 'src' is always followed by an ip address,
 we can use the 'index' function to find 'src' and add one to
 get the index position of our ip.
 """
 ipaddr_a = split_line_a[split_line_a.index('src')+1]
-ipaddr_b = split_line_b[split_line_b.index('src')+1]
+#ipaddr_b = split_line_b[split_line_b.index('src')+1]
 
 # Creates a sentence for each ip address.
 my_ip_a = 'Your %s ip is %s' % (ip_type_a, ipaddr_a)
-my_ip_b = 'Your %s ip is %s' % (ip_type_b, ipaddr_b)
+#my_ip_b = 'Your %s ip is %s' % (ip_type_b, ipaddr_b)
 
 # Creates the text, subject, 'from', and 'to' of the message.
-msg = MIMEText(my_ip_a + "\n" + my_ip_b)
+msg = MIMEText(my_ip_a)
 msg['Subject'] = 'IPs For ONUBajaDaq Raspberry Pi on %s' % today.strftime('%b %d %Y')
 msg['From'] = gmail_user
 msg['To'] = to
