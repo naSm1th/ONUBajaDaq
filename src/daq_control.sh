546fe4662a3f710de34245a8b2354cab76f10f0e
Ryan Carl <rcarl94@gmail.com>
2017-02-26 17:25:27 -0600

    Changed signal in daq_control to sigstop

--- src/daq_control.sh
+++ src/daq_control.sh
@@ -26,7 +26,7 @@ if [ -n $start ]; then
             # listen for button press
             read stop
             if [ -n $stop ]; then
-                kill -2 $pid
+                kill -19 $pid
                 wait $pid
                 # umount usb
                 $mount &
