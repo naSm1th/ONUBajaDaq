c451c387adc0e067df6e014486139cbc4a5b1991
Ryan Carl <rcarl94@gmail.com>
2017-02-26 18:35:00 -0600

    Changed back to current dir

--- src/logger.c
+++ src/logger.c
@@ -23,7 +23,7 @@
 
 #define MAX_LEN     100         /* max GPS string length */
 #define MAX_TOKENS  13          /* max num tokens in GPS string */
-#define LOG_DIR     "/mnt/bajadaq"         /* directory for logs (flash drive) */
+#define LOG_DIR     "."         /* directory for logs (flash drive) */
 
 int run;            // flag to run loop in usbdaq.c
 int counts[8];      // usbdaq shared array
