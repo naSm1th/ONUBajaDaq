fde2be565ad014cfb6659e7079c54df9737234b2
Ryan Carl <rcarl94@gmail.com>
2017-03-03 17:42:09 -0600

    fixed clean drive

--- src/clean_drive.sh
+++ src/clean_drive.sh
@@ -2,12 +2,12 @@
 
 ./mount.sh
 echo "Are you sure you want to remove the following?"
-ls /mnt/bajadaq/*/
+ls /mnt/bajadaq/1*/
 read ok
 while true; do
     case $ok in
-        [Yy]* ) rm -rf /mnt/bajadaq/*/; ./mount.sh; break;;
-        [Nn]* ) exit;;
+        [Yy]* ) rm -rf /mnt/bajadaq/1*/; ./mount.sh; break;;
+        [Nn]* ) ./mount.sh; exit;;
         * ) echo "(y)es or (n)o";;
     esac
 done
