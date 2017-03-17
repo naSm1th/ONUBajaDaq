b78593d9c82109dd96fa961e043294ce8521a764
Ryan Carl <rcarl94@gmail.com>
2017-03-16 22:11:49 -0500

    Fixed clean drive

--- src/clean_drive.sh
+++ src/clean_drive.sh
@@ -3,8 +3,8 @@
 ./mount.sh
 echo "Are you sure you want to remove the following?"
 ls /mnt/bajadaq/1*/
-read ok
 while true; do
+    read ok
     case $ok in
         [Yy]* ) rm -rf /mnt/bajadaq/1*/; ./mount.sh; break;;
         [Nn]* ) ./mount.sh; exit;;
